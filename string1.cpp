9f5a1879a1ff9729446179922de0a46cf3e8a025
ashmitchaurasiya-tech <ashmitchaurasiya9@gmail.com>
2026-09-09 23:10:14 +0530

    Strings intro

---
+++ string1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+    char str[] = {'a','b','c','\0'};
+    char str1[] = "Hello";//string literals
+
+    cout << strlen(str) << endl;//constant parameters
+    cout << str1[0] << endl;
+    cout << str1[1] << endl;
+    cout << str1[2] << endl;
+    cout << str1[3] << endl;
+    cout << str1[4] << endl;
+    cout << str1[5] << endl;
+
+}
