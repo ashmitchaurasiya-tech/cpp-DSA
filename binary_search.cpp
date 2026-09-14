0df01b603d5dd5b20505dfd0687eb9e79a409b24
ashmitchaurasiya-tech <ashmitchaurasiya9@gmail.com>
2026-09-14 23:43:45 +0530

    seacrching

---
+++ binary_search.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch(vector<int> arr , int target){
+    int st=0, end = arr.size()-1;
+
+    while(st<=end){
+        int mid = (st+end)/2;
+
+        if(target > arr[mid]){
+            st=mid+1;
+        }else if(target < arr[mid]){
+            end=mid-1;
+        }else{
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector <int> arr1 = {-1,0,3,4,5,9,12};
+    int tar1=12;
+
+    cout << binarySearch(arr1,tar1) << endl;
+
+    return 0;
+}
