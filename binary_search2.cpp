5470af6db37d53732119a0b06dd568d0e25c18b3
ashmitchaurasiya-tech <ashmitchaurasiya9@gmail.com>
2026-09-14 23:58:59 +0530

    optimize binary search

--- binary_search2.cpp
+++ binary_search2.cpp
@@ -6,7 +6,7 @@ int binarySearch(vector<int> arr , int target){
     int st=0, end = arr.size()-1;
 
     while(st<=end){
-        int mid = (st+end)/2;
+        int mid = st+(end-st)/2;
 
         if(target > arr[mid]){
             st=mid+1;
